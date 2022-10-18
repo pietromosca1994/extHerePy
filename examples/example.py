80a8daf4decdeb2947eeeda39948fdad1a52b011
Pietro Mosca <pietromosca1994@gmail.com>
2022-10-18 09:54:49 +0200

    added requirements.txt, setup.py

--- examples/example.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-PROJECT_PATH = os.getcwd()
-SOURCE_PATH = os.path.join(
-    "../", PROJECT_PATH,"extherepy"
-)
-sys.path.append(SOURCE_PATH)
-from extherepy.ext_geocoder_api import extGeocoderApi
